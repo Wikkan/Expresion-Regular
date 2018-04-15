4d700108cc673fa40e85a24c057fe8b6e5b9f85a
Wikkan <josuji-alfa@hotmail.com>
2018-04-15 09:42:32 -0500

    Numerar hojas

--- compi/compilador.py
+++ compi/compilador.py
@@ -1,3 +1,6 @@
+contadorHojas = 1
+
+# Estructura de Nodo
 class Node():
     def __init__(self, elemento):
         self.elemento = elemento
@@ -5,6 +8,9 @@ class Node():
         self.der = None
         self.hoja = True
         self.numeroHoja = None
+        self.anulable = False
+        self.primeraPos = []
+        self.ultimaPos = []
 
     def insertar(self, elemento, pos=0):
         if pos == 0:
@@ -37,17 +43,23 @@ class Node():
         return tiraIzq + " " + self.getElemento() + " " + tiraDer
 
     def nodosHoja(self):
+        global contadorHojas
+
         if self.izq is not None:
             self.izq.nodosHoja()
+
         if self.der is not None:
             self.der.nodosHoja()
+
         if self.hoja:
-            return [self]
+            self.numeroHoja = contadorHojas
+            contadorHojas += 1
 
     def getElemento(self):
         return self.elemento
 
 
+# Estructura de Arbol
 class Arbol():
     def __init__(self, elemento=None):
         if elemento is None:
@@ -67,7 +79,7 @@ class Arbol():
         else:
             return self.raiz.ird()
 
-    def imprimirHojas(self):
+    def buscarHojas(self):
         if self.raiz is None:
             return None
         else:
@@ -80,13 +92,13 @@ def concatenador(lista):
     i = 0
     while i != len(lista):
         if lista[i] in "+*":
-            if lista[i+1] not in "+*)#":
+            if lista[i+1] not in "+*)#]":
                 listaAux.append(lista[i])
                 listaAux.append(".")
             else:
                 listaAux.append(lista[i])
-        elif lista[i] not in "+*(#|":
-            if lista[i+1] == "(" or lista[i+1] not in "+*)|":
+        elif lista[i] not in "+*(#|[":
+            if lista[i+1] == "(" or lista[i] == "[" or lista[i+1] not in "+*)|]":
                 listaAux.append(lista[i])
                 listaAux.append(".")
             else:
@@ -96,25 +108,28 @@ def concatenador(lista):
         i += 1
     return listaAux
 
+
 # Borra los espacios en blanco y retorna una lista con los tokens
 def depurar(cadena):
     lista = []
     i = 0
     while i != len(cadena):
         if cadena[i] == "'":
-            constante = ""
+            lista.append('(')
             i += 1
             while cadena[i] != "'":
-                constante += cadena[i]
+                lista.append(cadena[i])
                 i += 1
-            lista.append(constante)
+            lista.append(')')
         elif cadena[i] in "+*[]|()":
             lista.append(cadena[i])
         i += 1
     lista.append("#")
+    print(lista)
     return enlistar(concatenador(lista))
 
-# Vuelve los valores dentro de parentesis una lista en la lista de tokens
+
+# Vuelve los valores dentro de parentesis y corchetes, una lista en la lista de tokens
 def enlistar(lista):
     listaSalida = []
     listaAux = []
@@ -123,16 +138,18 @@ def enlistar(lista):
     pos = []
 
     for x in lista:
-        if x == "(":
+        if x == "(" or x == "[":
             abierto += 1
             pos.append(i)
             i += 1
-        elif x == ")":
+        elif x == ")" or x == "]":
             if abierto == 1:
                 for y in lista[pos.pop()+1:i]:
                     listaAux.append(y)
                 lista = lista[i+1:]
                 listaSalida.append(listaAux)
+                if x == "]":
+                    listaSalida.append('?')
                 i = 0
                 pos.clear()
             elif abierto > 1:
@@ -141,7 +158,11 @@ def enlistar(lista):
                     listaAux.append(y)
                 lista = lista[:v] + lista[i+1:]
                 lista.insert(v, listaAux)
-                i = v + 1
+                if x == "]":
+                    lista.insert(v+1, '?')
+                    i = v + 2
+                else:
+                    i = v + 1
             abierto -= 1
             listaAux = []
         elif abierto == 0:
@@ -151,9 +172,10 @@ def enlistar(lista):
             i += 1
     return listaSalida
 
+
 # Ordena la lista por prioridad de símbolos
 def ordenarLista(lista):
-    listaSimbolos = [".", "|", "*", "+"]
+    listaSimbolos = [".", "|", "*", "+", "?"]
     i = len(lista)-1
 
     if len(lista) == 1:
@@ -169,7 +191,8 @@ def ordenarLista(lista):
     else:
         return [None]
 
-# Crea el arbol sintáctico
+
+# Crea el árbol sintáctico
 def crearArbol(lista, arbol, pos=0):
     if len(lista) == 3:
         arbol.insertar(lista[0], pos-1)
@@ -178,20 +201,23 @@ def crearArbol(lista, arbol, pos=0):
     elif len(lista) == 1:
         arbol.insertar(lista[0], pos-1)
 
+
 # Función inicial
 def er(cadena):
     arbol = Arbol()
     listaTokens = depurar(cadena)
-    #print(listaTokens)
+    print(listaTokens)
     listaPrioridad = ordenarLista(listaTokens)
-    #print(listaPrioridad)
+    print(listaPrioridad)
     crearArbol(listaPrioridad, arbol)
-    #arbol.recorridoIRD()
-    return arbol.imprimirHojas()
+    print(arbol.recorridoIRD())
+    arbol.buscarHojas()
+    return arbol
+
 
 # Pruebas
 #print(er("'hola'+('2'|'a') 'a-z'"))
-print(er("('a'|'b')*'a''b''b'"))
-#print(er("('a'|'b')*'hola'+('a'('a'|'b')+)*"))
+print(er("('a'|'b')*'abb'"))
+#print(er("('a'|'b')*'hola'+('a'('a'|'b')+['a'])*"))
 #print(er("('a'|'b')('b'|'a')"))
 #print(er("'a'+('ab'|'ba')*'cvd''ascd'*('a'*('a'|('b'|'c')*)+)"))
